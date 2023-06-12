c5ea5cd8ba4654e3a8287ad05e9db19bd958a8be
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2023-06-12 10:19:48 -0400

    Forgot to add mart model

---
+++ models/mart/tpch_locations.sql
@@ -0,0 +1,36 @@
+{{ 
+    config(
+        tags = ['location'],
+        materialized = 'view'
+    )
+}}
+
+with nation as (
+    
+    select * from {{ ref('stg_nation') }}
+
+),
+region as (
+    
+    select * from {{ ref('stg_region') }}
+
+),
+
+final as (
+    select
+        nation.nation_key,
+        nation.name as nation,
+        nation.region_key,
+        region.name as region
+    from
+        nation
+    inner join region
+            on nation.region_key = region.region_key
+)
+
+select
+    *
+from
+    final
+order by
+    nation_key
